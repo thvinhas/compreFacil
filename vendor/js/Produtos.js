29f1b00aedf64e21d47513ba59969dddacbcea25
Thiago Vinhas <vinhas.thiago@gmail.com>
2018-05-14 17:47:21 -0300

    inicio da tea de carrinho de compra

--- vendor/js/Produtos.js
+++ vendor/js/Produtos.js
@@ -9,21 +9,19 @@ function exibirTodosProdutos() {
             lojas.forEach(function (loja){
                 let produtos = loja.child('produtos');
                 produtos.forEach(function (produto) {
-                    html +=  "<div class=\"col-lg-4 col-md-6 mb-4\">" +
-                        "              <div class=\"card h-100\">" +
-                        "                <a href=\"#\"><img class=\"card-img-top\" src=\"http://placehold.it/700x400\" alt=\"\"></a>" +
-                        "                <div class=\"card-body\">" +
-                        "                  <h4 class=\"card-title\">" +
-                        "                    <a onclick='exibirProduto(" + loja.key + "," + produto.key + ")'>"+produto.val().nome+"</a>" +
-                        "                  </h4>" +
-                        "                  <h5>$24.99</h5>" +
-                        "                  <p class=\"card-text\">"+produto.val().descricao+"</p>" +
-                        "                </div>" +
-                        "                <div class=\"card-footer\">" +
-                        "                  <small class=\"text-muted\">&#9733; &#9733; &#9733; &#9733; &#9734;</small>" +
-                        "                </div>" +
-                        "                </div>" +
-                        "              </div>";
+
+                    html +="<div class=\"col-lg-4 col-md-6 mb-4\">" +
+                    			"<a onclick='exibirProduto(" + loja.key + "," + produto.key + ")'>"+
+                        			"<div class=\"card h-100\">" +
+                        				"<img class=\"card-img-top\" src=\"http://placehold.it/700x400\" alt=\"\">" +
+                   						"<div class=\"card-body\">" +
+                    						"<h4 class=\"card-title\">"+ produto.val().nome + "</h4>" +
+                    						"<h5>$24.99</h5>" +
+                    						"<p class=\"card-text\">"+produto.val().descricao+"</p>" +
+                    					"</div>" +
+                        			"</div>" +
+                    			"</a>" +
+                			"</div>";
                 });
             });
             $('.produtos').html(html);
